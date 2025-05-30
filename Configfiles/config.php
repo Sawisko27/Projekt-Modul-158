c8ccfd900cb7bdce147ef96b509f8e296687ba9a
Sawisko27 <savvas.ioannidis@edu.gbssg.ch>
2025-05-30 15:16:33 +0200

    Update config.php

--- Configfiles/config.php
+++ Configfiles/config.php
@@ -1,7 +1,11 @@
 <?php  // Moodle configuration file
+
+// Grundkonfiguration
 unset($CFG);
 global $CFG;
 $CFG = new stdClass();
+
+// Datenbankeinstellungen
 $CFG->dbtype    = 'mariadb';
 $CFG->dblibrary = 'native';
 $CFG->dbhost    = 'mariadb';
@@ -9,17 +13,22 @@ $CFG->dbname    = 'moodle';
 $CFG->dbuser    = 'moodle';
 $CFG->dbpass    = 'deinpasswort';
 $CFG->prefix    = 'mdl_';
+
+// Erweiterte DB-Optionen
 $CFG->dboptions = array (
   'dbpersist' => 0,
   'dbport' => '',
   'dbsocket' => '',
   'dbcollation' => 'utf8mb4_unicode_ci',
 );
+
+// Pfade & Verzeichnisse
 $CFG->wwwroot   = 'http://localhost';
 $CFG->dataroot  = '/var/www/moodledata';
 $CFG->admin     = 'admin';
 $CFG->directorypermissions = 0777;
+
+// Moodle Bootstrap
 require_once(__DIR__ . '/lib/setup.php');
-// There is no php closing tag in this file,
-// it is intentional because it prevents trailing whitespace problems!
- 
+
+// Kein PHP-Schluss-Tag zur Vermeidung von Whitespace-Problemen
